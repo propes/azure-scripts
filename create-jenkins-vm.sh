85267319d7a6f483890d17706c1892974d978890
propes <>
2019-03-15 11:00:36 +0000

    jenkins vm script

---
+++ create-jenkins-vm.sh
@@ -0,0 +1,8 @@
+rg=jenkins-rg
+vm=jenkins-vm1
+az vm create --resource-group $rg /
+  --name $vm --image UbuntuLTS /
+  --admin-username azureuser /
+  --generate-ssh-keys /
+  --custom-data cloud-init-jenkins.txt
+az vm open-port --resource-group $rg --name $vm --port  8080
