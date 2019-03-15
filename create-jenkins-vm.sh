7839f82c89df5388db7168cc7d373d03778b5bd0
propes <>
2019-03-15 11:06:42 +0000

    amend jenkins vm script

--- create-jenkins-vm.sh
+++ create-jenkins-vm.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
+# Sets up a jenkins vm in azure.
+
 rg=jenkins-rg
 vm=jenkins-vm1
-az vm create --resource-group $rg /
-  --name $vm --image UbuntuLTS /
+
+az vm create /
+  --resource-group $rg /
+  --name $vm /
+  --image UbuntuLTS /
   --admin-username azureuser /
   --generate-ssh-keys /
   --custom-data cloud-init-jenkins.txt
+
 az vm open-port --resource-group $rg --name $vm --port  8080
